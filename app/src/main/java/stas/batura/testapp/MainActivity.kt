6839a813975ac9ea6d2ef9fdc2d28db7000e63de
home <seeyou85@gmail.com>
2021-06-21 13:34:20 +0400

    header of appbar

--- app/src/main/java/stas/batura/testapp/MainActivity.kt
+++ app/src/main/java/stas/batura/testapp/MainActivity.kt
@@ -25,10 +25,10 @@ class MainActivity : AppCompatActivity() {
 //        navView.setupWithNavController(navController)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_scrolling, menu)
-        return true
-    }
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        menuInflater.inflate(R.menu.menu_scrolling, menu)
+//        return true
+//    }
 
 }
